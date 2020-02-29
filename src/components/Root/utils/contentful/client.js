9cd3025f921177de8a84f744f3b9545cfb04a695
Caroline Nolasco <cnolascoix@gmail.com>
2020-02-28 23:30:11 -0800

    edit gitignore remove contentful

---
+++ src/components/Root/utils/contentful/client.js
@@ -0,0 +1,8 @@
+import * as contentful from 'contentful';
+
+    const client = contentful.createClient({
+        space: "6jizbkvr12of",
+        accessToken: "L60lNNHrqGGj8Y3zXd-cnI58eWKKwwcSCxyt1Z9Pm2Q"
+        });
+
+export default client;
