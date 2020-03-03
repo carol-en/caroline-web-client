d3361c56c0f8212fcae6de427804d70b5ffb2600
Caroline Nolasco <cnolascoix@gmail.com>
2020-03-03 13:09:20 -0800

    remove fetch from home

--- src/components/Root/Home/Home.js
+++ src/components/Root/Home/Home.js
@@ -4,16 +4,7 @@ import Logo from "../Logo";
 import "./home.scss";
 
 class Home extends Component {
-    // componentDidMount = () => {
-    //     fetch("https://art-marks-client.herokuapp.com/", {mode: 'no-cors'})
-    //      .then(res => console.log("woke up art marks"));
 
-    //      fetch("https://youtube-api-ga.herokuapp.com/", {mode: 'no-cors'})
-    //      .then(res => console.log("woke up youtube api"));
-
-    //      fetch("https://art-planner-heroku.herokuapp.com/planner", {mode: 'no-cors'})
-    //      .then(res => console.log("woke up art planner"));
-    // }
     render() {
         return (
         <main className="home">
