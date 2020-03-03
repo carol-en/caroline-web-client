df141e5fe32a01839bacd821f879e7e658096b32
Caroline Nolasco <cnolascoix@gmail.com>
2020-03-03 14:40:13 -0800

    moved project fetches to root

--- src/components/Root/Home/Home.js
+++ src/components/Root/Home/Home.js
@@ -5,22 +5,6 @@ import "./home.scss";
 
 class Home extends Component {
 
-    componentDidMount = () => {
-        fetch("https://art-marks-server.herokuapp.com/my_bookmarks", {mode: 'no-cors'})
-         .then(res => console.log("woke up art marks server"));
-
-        fetch("https://art-marks-client.herokuapp.com/", {mode: 'no-cors'})
-         .then(res => console.log("woke up art marks"));
-
-         fetch("https://youtube-api-ga.herokuapp.com/", {mode: 'no-cors'})
-         .then(res => console.log("woke up youtube api"));
-
-         fetch("https://art-planner-heroku.herokuapp.com/planner", {mode: 'no-cors'})
-         .then(res => console.log("woke up art planner"));
-
-         fetch("https://left4dead-mini-game.herokuapp.com/", {mode: 'no-cors'})
-         .then(res => console.log("woke up art left4dead mini game"));
-    }
     render() {
         return (
         <main className="home">
--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -7,6 +7,22 @@ import Footer from "./Footer";
 
 
 class Root extends Component {
+    componentDidMount = () => {
+        fetch("https://art-marks-server.herokuapp.com/my_bookmarks", {mode: 'no-cors'})
+         .then(res => console.log("woke up art marks server"));
+
+        fetch("https://art-marks-client.herokuapp.com/", {mode: 'no-cors'})
+         .then(res => console.log("woke up art marks"));
+
+         fetch("https://youtube-api-ga.herokuapp.com/", {mode: 'no-cors'})
+         .then(res => console.log("woke up youtube api"));
+
+         fetch("https://art-planner-heroku.herokuapp.com/planner", {mode: 'no-cors'})
+         .then(res => console.log("woke up art planner"));
+
+         fetch("https://left4dead-mini-game.herokuapp.com/", {mode: 'no-cors'})
+         .then(res => console.log("woke up art left4dead mini game"));
+    }
     render() {
         return (
             <>
